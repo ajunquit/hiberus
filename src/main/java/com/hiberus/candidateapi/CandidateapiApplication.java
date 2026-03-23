54feb5a7050a8834a692e8c8899c2b9b8becdfe1
Alejandro Junqui <ajunquit@gmail.com>
2026-03-22 21:55:29 -0500

    initial commit

---
+++ src/main/java/com/hiberus/candidateapi/CandidateapiApplication.java
@@ -0,0 +1,13 @@
+package com.hiberus.candidateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateapiApplication.class, args);
+	}
+
+}
